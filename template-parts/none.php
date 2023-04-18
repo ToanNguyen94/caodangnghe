c3e5f1659fb88cea8d54e8f2ee966284a17d7bd1
ToanNguyen94 <nguyenthetoan.ntt@gmail.com>
2023-04-18 11:48:08 +0700

    fix search-page

--- template-parts/none.php
+++ template-parts/none.php
@@ -1,13 +1,19 @@
 <section class="no-results not-found">
-    <h1 class="page-title"><?php esc_html_e('Nothing Found', 'independentlink'); ?></h1>
-
-    <div class="page-content">
-        <?php if (is_search() ) : ?>
-            <p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'independentlink'); ?></p>
-            <?php get_search_form(); ?>
-        <?php else : ?>
-            <p><?php esc_html_e("It seems we can't find what you're looking for. Perhaps searching can help.", 'independentlink'); ?></p>
-            <?php get_search_form(); ?>
-        <?php endif; ?>
+    <div class="container">
+        <div class="page-content page-search-notfound ">
+            <h2 class="title"><?php esc_html_e('Nothing Found', 'xadventures'); ?></h2>
+            <?php if (is_search()) : ?>
+                <p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'xadventures'); ?></p>
+                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mx-auto">
+                    <form role="search" method="get" id="searchform" class="searchForm" action="<?= home_url('/') ?>">
+                        <input type="text" value="<?php get_search_query() ?>" name="s" id="s" placeholder="Nhập từ khóa tìm kiếm" />
+                        <input type="submit" value=""></input>
+                    </form>
+                </div>
+            <?php else : ?>
+                <p><?php esc_html_e("It seems we can't find what you're looking for. Perhaps searching can help.", 'xadventures'); ?></p>
+                <?php get_search_form(); ?>
+            <?php endif; ?>
+        </div>
     </div>
 </section>
